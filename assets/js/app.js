81982ae00eda0bed559009c67187aa54ab877a54
Neiza Nunez <neizapf@gmail.com>
2017-04-17 17:54:19 -0500

    elimina fotos

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var panda1 = document.getElementById("panda1");
+var panda2 = document.getElementById("panda2");
+var panda3 = document.getElementById("panda3");
+var panda4 = document.getElementById("panda4");
+var span1 = document.getElementById("span1");
+var span2 = document.getElementById("span2");
+var span3 = document.getElementById("span3");
+var span4 = document.getElementById("span4");
+var origen = document.getElementById("origen");
+var extension = document.getElementById("extension");
+var eliminar =  document.getElementById("eliminar");
+
+
+
+span1.addEventListener("click", function(){
+      panda1.style.display = "none";
+});
+
+span2.addEventListener("click", function(){
+      panda2.style.display = "none";
+});
+span3.addEventListener("click", function(){
+      panda3.style.display = "none";
+});
+span4.addEventListener("click", function(){
+      panda4.style.display = "none";
+});
